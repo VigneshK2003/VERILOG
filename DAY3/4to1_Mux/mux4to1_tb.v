a608b320c3fc21a3e27b23bbbcf4e7e04fe79a47
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2025-10-14 12:44:46 +0530

    Update mux4to1_tb.v

--- DAY3/4to1_Mux/mux4to1_tb.v
+++ DAY3/4to1_Mux/mux4to1_tb.v
@@ -3,9 +3,6 @@ module mux4to1_tb();
      wire y;
   mux4to1 uut(.s0(s0), .s1(s1), .i0(i0), .i1(i1), .i2(i2),.i3(i3), .y(y));
   initial begin
-      //i0 = 0; i1 = 0; i2 = 0; i3 = 0;
-        //s0 = 0; s1 = 0;
-
         i0=1; i1=0; i2=0; i3=0; s0=0; s1=0; #10
         i0=0; i1=1; i2=0; i3=0; s0=0; s1=1; #10 
         i0=0; i1=0; i2=1; i3=0; s0=1; s1=0; #10
